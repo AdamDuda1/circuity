5c6995066ed5197e3619b7fd99289626f17d7502
Adam Duda <adam.duda@post.com>
2026-02-23 23:18:35 +0100

    Add position editing in component properties panel

--- src/app/component-properties/component-properties.ts
+++ src/app/component-properties/component-properties.ts
@@ -31,9 +31,9 @@ export class Category {}
 export class ComponentProperties {
 	constructor(public globals: Globals) {}
 
-	change_name(event: Event) {
-		const input = event.target as HTMLInputElement;
-		this.globals.simulation.circuitComponents()[this.globals.selected].name = input.value;
-		console.log(input.value);
-	}
+	change_name(event: Event) { this.globals.simulation.circuitComponents()[this.globals.selected].name = (event.target as HTMLInputElement).value; }
+
+	change_x(event: Event) { this.globals.simulation.circuitComponents()[this.globals.selected].x = Number((event.target as HTMLInputElement).value); }
+
+	change_y(event: Event) { this.globals.simulation.circuitComponents()[this.globals.selected].y = Number((event.target as HTMLInputElement).value); }
 }
